c2ac277f26f63bf4e7cc19473e2ff2de000a4831
Ubuntu <ubuntu@ip-172-31-24-55.ec2.internal>
2020-05-19 18:29:03 +0000

    assigment two submission checking in to git

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -11,18 +11,18 @@ public class EnvControllerTest {
     @Test
     public void getEnv() throws Exception {
         EnvController controller = new EnvController(
-            "8675",
-            "12G",
-            "34",
-            "123.sesame.street"
+            "NOT SET",
+            "NOT SET",
+            "NOT SET",
+            "NOT SET"
         );
 
         Map<String, String> env = controller.getEnv();
 
-        assertThat(env.get("PORT")).isEqualTo("8675");
-        assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
-        assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
-        assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("123.sesame.street");
+        assertThat(env.get("PORT")).isEqualTo("NOT SET");
+        assertThat(env.get("MEMORY_LIMIT")).isEqualTo("NOT SET");
+        assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("NOT SET");
+        assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("NOT SET");
     }
 
 }
